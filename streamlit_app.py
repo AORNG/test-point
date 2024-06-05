57b53d5913eedd80c95d7a437812b487d70e868b
AORNG <167181348+AORNG@users.noreply.github.com>
2024-06-05 07:41:40 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,39 +1,17 @@
 import streamlit as st
-st.title(":red[歴史単語]$$writter$$")
-st.write("$こんにちは。皆さん！今日も元気ですか？ $")
-st.write("$私は元気です！さあ勉強を開始しましょう！$")
-if st.button("$Let's play study!$"):
-    st.write("_the new game!!_")
+import folium
 
-@st.cache
-def load_data():
-    return pd.read_excel("歴史単語～世界史～.xlsx")
+def main():
+    st.title("Streamlitで地図を表示する")
 
-words_df = load_data()
+    # 地図の中心座標を設定
+    center = [35.6895, 139.6917]  # 例：東京の緯度経度
 
-# ガチャ機能
-if st.button('ガチャを引く！'):
-    rarity_probs = {
-        'N': 0.4,
-        'R': 0.3,
-        'SR': 0.2,
-        'SSR': 0.1
-    }
-    chosen_rarity = np.random.choice(list(rarity_probs.keys()), p=list(rarity_probs.values()))
-    subset_df = words_df[words_df['レア度'] == chosen_rarity]
-    selected_word = subset_df.sample().iloc[0]
-    
-    # セッションステートに選択された単語を保存
-    st.session_state.selected_word = selected_word
-    st.session_state.display_meaning = False
+    # Foliumのマップオブジェクトを作成
+    m = folium.Map(location=center, zoom_start=10)
 
-if 'selected_word' in st.session_state:
-    st.header(f"単語名: {st.session_state.selected_word['単語']}")
-    st.subheader(f"レア度: {st.session_state.selected_word['レア度']}")
+    # StreamlitでFoliumの地図を表示
+    folium_static(m)
 
-    # 意味を確認するボタンを追加
-    if st.button('意味を確認する'):
-        st.session_state.display_meaning = True
-
-    if st.session_state.display_meaning:
-        st.write(f"意味: {st.session_state.selected_word['意味']}")
+if __name__ == "__main__":
+    main()
