5a2e3906fd9d3b161ff4f35758c1e3aa39eb38b7
AORNG <167181348+AORNG@users.noreply.github.com>
2024-07-18 08:30:48 +0000

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,11 @@ import plotly.express as px
 
 st.title("5教科の点数のレーダーチャート")
 
-A=st.text_number("国語")
-B=st.text_number("数学")
-C=st.text_number("理科")
-D=st.text_number("社会")
-E=st.text_number("英語")
+A=st.number_input("国語")
+B=st.number_input("数学")
+C=st.number_input("理科")
+D=st.number_input("社会")
+E=st.number_input("英語")
 
 df=pd.DataFrame({
     "教科":["国語","数学","理科","社会","英語"],
