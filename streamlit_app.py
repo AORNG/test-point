6cb9697e17534fd8b08df9f7a6c03b14a8cfa53a
AORNG <167181348+AORNG@users.noreply.github.com>
2024-07-18 08:45:03 +0000

    t

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,15 +21,16 @@ fig=px.bar(df,x="教科",y="点数")
 if st.button("棒グラフ"):
     st.plotly_chart(fig)
 
-values=([A,B,C,D,E])
-labels = [f"データ{i}" for i in range(1, len(values)+1)]
 # 多角形を閉じるためにデータの最後に最初の値を追加する。
 radar_values = np.concatenate([values, [values[0]]])
 # プロットする角度を生成する。
 angles = np.linspace(0, 2 * np.pi, len(labels) + 1, endpoint=True)
+# メモリ軸の生成
+rgrids = [0, 20, 40, 60, 80, 100]
+
 
 fig = plt.figure(facecolor="w")
-# 極座標でaxを作成。
+# 極座標でaxを作成
 ax = fig.add_subplot(1, 1, 1, polar=True)
 # レーダーチャートの線を引く
 ax.plot(angles, radar_values)
@@ -37,6 +38,27 @@ ax.plot(angles, radar_values)
 ax.fill(angles, radar_values, alpha=0.2)
 # 項目ラベルの表示
 ax.set_thetagrids(angles[:-1] * 180 / np.pi, labels)
+# 円形の目盛線を消す
+ax.set_rgrids([])
+# 一番外側の円を消す
+ax.spines['polar'].set_visible(False)
+# 始点を上(北)に変更
+ax.set_theta_zero_location("N")
+# 時計回りに変更(デフォルトの逆回り)
+ax.set_theta_direction(-1)
+
+# 多角形の目盛線を引く
+for grid_value in rgrids:
+    grid_values = [grid_value] * (len(labels)+1)
+    ax.plot(angles, grid_values, color="gray",  linewidth=0.5)
+
+# メモリの値を表示する
+for t in rgrids:
+    # xが偏角、yが絶対値でテキストの表示場所が指定される
+    ax.text(x=0, y=t, s=t)
+
+# rの範囲を指定
+ax.set_rlim([min(rgrids), max(rgrids)])
 
 ax.set_title("レーダーチャート", pad=20)
 plt.show()
