b5be574d1c769ab630d06aa297917b8b829d6be6
AORNG <167181348+AORNG@users.noreply.github.com>
2024-07-18 07:47:58 +0000

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ import numpy as np
 
 st.title("5教科の点数のレーダーチャート")
 
-A=st.number_input()
-B=st.number_input()
-C=st.number_input()
-D=st.number_input()
-E=st.number_input()
+A=st.text.input()
+B=st.text.input()
+C=st.text.input()
+D=st.text.input()
+E=st.text.input()
