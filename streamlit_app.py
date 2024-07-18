a2f53d5ac0691aa9ce805abf9e35126c9c3379cb
AORNG <167181348+AORNG@users.noreply.github.com>
2024-07-18 07:53:45 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,9 @@ B=st.text_input("数学")
 C=st.text_input("理科")
 D=st.text_input("社会")
 E=st.text_input("英語")
+dataframe=(A,B,C,D,E)
+columns = ('col %d' % i
+    for i in range(5))
+dataframe
+st.write('This is a line_chart.')
+st.line_chart(dataframe)
