154eb8ada10d05c89ff89c82424f00d0f0301844
AORNG <167181348+AORNG@users.noreply.github.com>
2024-07-18 09:01:11 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
 import numpy as np
+import japanize_matplotlib
 
 st.title("5教科の点数のレーダーチャート")
 
@@ -21,22 +22,30 @@ fig=px.bar(df,x="教科",y="点数")
 if st.button("棒グラフ"):
     st.plotly_chart(fig)
 
-values=([A,B,C,D,E])
-labels = [f"データ{i}" for i in range(1, len(values)+1)]
-# 多角形を閉じるためにデータの最後に最初の値を追加する。
-radar_values = np.concatenate([values, [values[0]]])
-# プロットする角度を生成する。
-angles = np.linspace(0, 2 * np.pi, len(labels) + 1, endpoint=True)
+# クラスのlabel_listと精度の値を設定
+label_list = ['車', '人', '自転車', '犬', '猫', 'バス', 'トラック', '信号']
+acc_list = [0.90, 0.85, 0.75, 0.80, 0.95, 0.70, 0.85, 0.88]  # 例えば、これらは各クラスの精度を表します。
+acc_list += acc_list[:1]  # チャートを閉じるために最初の値を末尾に追加
 
-fig = plt.figure(facecolor="w")
-# 極座標でaxを作成。
-ax = fig.add_subplot(1, 1, 1, polar=True)
-# レーダーチャートの線を引く
-ax.plot(angles, radar_values)
-#　レーダーチャートの内側を塗りつぶす
-ax.fill(angles, radar_values, alpha=0.2)
-# 項目ラベルの表示
-ax.set_thetagrids(angles[:-1] * 180 / np.pi, labels)
+acc_list_2 = [0.50, 0.55, 0.85, 0.60, 0.85, 0.50, 0.65, 0.48]  # 例えば、これらは各クラスの精度を表します。
+acc_list_2 += acc_list_2[:1]  # チャートを閉じるために最初の値を末尾に追加
 
-ax.set_title("レーダーチャート", pad=20)
-plt.show(fig)
+# レーダーチャートを描画するためのangle_listを計算
+angle_list = [n / float(len(label_list)) * 2 * np.pi for n in range(len(label_list))]
+angle_list += angle_list[:1]
+
+# matplotlibでレーダーチャートを描画
+fig, ax = plt.subplots(figsize=(8, 8), subplot_kw=dict(polar=True))
+plt.xticks(angle_list[:-1], label_list, color='grey', size=12)
+
+# # プロット線を描画
+ax.plot(angle_list, acc_list, linewidth=1, linestyle='solid', label='Model A')
+ax.fill(angle_list, acc_list, 'blue', alpha=0.1)
+
+ax.plot(angle_list, acc_list_2, linewidth=1, linestyle='solid', label='Model B')
+ax.fill(angle_list, acc_list_2, 'red', alpha=0.1)
+
+
+# # チャートを表示
+plt.legend(loc='upper right')
+plt.show()
