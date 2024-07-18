69d251727091e35b7a31e1462d5868e481a8f7b8
AORNG <167181348+AORNG@users.noreply.github.com>
2024-07-18 08:26:02 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+
 st.title("5教科の点数のレーダーチャート")
 
 A=st.text_number("国語")
@@ -9,8 +10,10 @@ C=st.text_number("理科")
 D=st.text_number("社会")
 E=st.text_number("英語")
 
-df = pd.DataFrame(dict(
-    r=[1, 5, 2, 2, 3],
-    theta=[A,B,C,D,E]))
-fig = px.line_polar(df, r='r', theta='theta', line_close=True)
-fig.show()
+df=pd.DataFrame({
+    "教科":["国語","数学","理科","社会","英語"],
+    "点数":[A,B,C,D,E]
+})
+fig=px.bar(df,x="教科",y="点数")
+
+st.plotly_chart(fig)
