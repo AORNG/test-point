a079557e836ebe028a39f36bf8ad4e7c198366b5
AORNG <167181348+AORNG@users.noreply.github.com>
2024-07-18 07:58:07 +0000

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 st.title("5教科の点数のレーダーチャート")
 
@@ -9,9 +10,5 @@ B=st.text_input("数学")
 C=st.text_input("理科")
 D=st.text_input("社会")
 E=st.text_input("英語")
-dataframe=(A,B,C,D,E)
-columns = ('col %d' % i
-    for i in range(5))
-dataframe
-st.write('This is a line_chart.')
-st.line_chart(dataframe)
+values = np.array([A,B,C,D,E])
+labels = [f"データ{i}" for i in range(1, len(values)+1)]
