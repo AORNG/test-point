1923519be0d79c87bcf46a0390ff4aa240fc09e5
AORNG <167181348+AORNG@users.noreply.github.com>
2024-07-18 08:01:26 +0000

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,10 @@ import numpy as np
 
 st.title("5教科の点数のレーダーチャート")
 
-A=st.text_input("国語")
-B=st.text_input("数学")
-C=st.text_input("理科")
-D=st.text_input("社会")
-E=st.text_input("英語")
+A=st.text_number("国語")
+B=st.text_number("数学")
+C=st.text_number("理科")
+D=st.text_number("社会")
+E=st.text_number("英語")
 values = np.array([A,B,C,D,E])
 labels = [f"データ{i}" for i in range(1, len(values)+1)]
